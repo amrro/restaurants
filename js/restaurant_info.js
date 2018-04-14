256fdbee2c10d13e55faff8a11bc4873b4b24652
Amro Elghobary <amr.elghobary@gmail.com>
2018-04-14 18:47:28 +0200

    Fix for rubric

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -152,6 +152,7 @@ createReviewHTML = (review) => {
  */
 fillBreadcrumb = (restaurant = self.restaurant) => {
     const breadcrumb = document.getElementById('breadcrumb');
+    breadcrumb.setAttribute("aria-current", restaurant.name.toString());
     const li = document.createElement('li');
     li.innerHTML = restaurant.name;
     breadcrumb.appendChild(li);
