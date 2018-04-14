b992bf00ff2adbe697f8c4d6c22bf19ea25b65c8
Amro Elghobary <amr.elghobary@gmail.com>
2018-04-14 04:50:14 +0200

    I don't remember.

--- js/main.js
+++ js/main.js
@@ -149,6 +149,7 @@ createRestaurantHTML = (restaurant) => {
     const image = document.createElement('img');
     image.className = 'restaurant-img';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
+    image.alt = `An image for ${restaurant.name}.`;
     li.append(image);
 
     // adding restaurant's name to card
